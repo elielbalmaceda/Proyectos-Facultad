00c4d287f2dd7de4dff329318c9407aed07ad9d4
Eliel Balmaceda <Eliel Balmaceda>
2023-09-30 19:34:11 -0300

    30-09

---
+++ Back-Front CMP/ejemplo01/src/app/Modelo/Inventario.ts
@@ -0,0 +1,7 @@
+export class Inventario{
+    idNumInventario:number | undefined;
+    nombre: String | undefined;
+    estado: String | undefined;
+    lugar: String | undefined;
+    usuario: String | undefined;
+}
--- Back-Front CMP/ejemplo01/src/app/Service/service.service.ts
+++ Back-Front CMP/ejemplo01/src/app/Service/service.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient } from '@angular/common/http'
+import { Inventario } from '../Modelo/Inventario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  Url = 'http://localhost:8080/ejemplo01/inventario';
+
+  getInventario(){
+    return this.http.get<Inventario[]>(this.Url)
+  }
 }
