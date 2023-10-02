f4a0fc5a8c10f2afef66f07b1ec11960f1428988
Eliel Balmaceda <Eliel Balmaceda>
2023-10-02 15:59:12 -0300

    2-10

---
+++ Angular Material/material/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Angular Material
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+//Componentes
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
